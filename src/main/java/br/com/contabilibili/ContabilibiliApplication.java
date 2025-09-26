f5385bd4d44fea6620765c06b5206a886eb665cf
lucasmarques594 <demordevil594@gmail.com>
2025-09-26 12:12:24 -0300

    refactor: switch from Spring Boot to JavaFX application launcher

--- src/main/java/br/com/contabilibili/ContabilibiliApplication.java
+++ src/main/java/br/com/contabilibili/ContabilibiliApplication.java
@@ -1,13 +1,12 @@
 package br.com.contabilibili;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.contabilibili.ui.JavaFxApplication;
+import javafx.application.Application;
 
 @SpringBootApplication
 public class ContabilibiliApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ContabilibiliApplication.class, args);
+		Application.launch(JavaFxApplication.class, args);
 	}
-
 }
