cad13e9dcb06af062ee54e6e34a32a253b86bf93
lucasmarques594 <demordevil594@gmail.com>
2025-09-26 13:54:05 -0300

    feat(repository): add findAll and deleteById methods to CartorioRepository
    Add missing CRUD operations to provide complete repository functionality

--- src/main/java/br/com/contabilibili/domain/repository/CartorioRepository.java
+++ src/main/java/br/com/contabilibili/domain/repository/CartorioRepository.java
@@ -1,10 +1,14 @@
 package br.com.contabilibili.domain.repository;
 
 import br.com.contabilibili.domain.model.Cartorio;
+import java.util.List;
 import java.util.Optional;
 
 public interface CartorioRepository {
     Cartorio save(Cartorio cartorio);
-
     Optional<Cartorio> findById(Long id);
+
+    List<Cartorio> findAll();
+
+    void deleteById(Long id);
 }
