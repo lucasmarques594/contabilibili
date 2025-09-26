502b32cc614467b340656c53d918b7dc88dc5b02
lucasmarques594 <demordevil594@gmail.com>
2025-09-26 13:52:02 -0300

    feat: add MainViewController for JavaFX application navigation

---
+++ src/main/java/br/com/contabilibili/ui/controller/MainViewController.java
@@ -0,0 +1,47 @@
+package br.com.contabilibili.ui.controller;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class MainViewController {
+
+    @FXML
+    private BorderPane mainPane;
+
+    private final ApplicationContext springContext;
+
+    public MainViewController(ApplicationContext springContext) {
+        this.springContext = springContext;
+    }
+
+    @FXML
+    void abrirTelaCartorios(ActionEvent event) {
+        carregarView("CartorioView");
+    }
+
+    @FXML
+    void fecharAplicacao(ActionEvent event) {
+        Platform.exit();
+    }
+
+    private void carregarView(String viewNome) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/br/com/contabilibili/ui/view/" + viewNome + ".fxml"));
+            loader.setControllerFactory(springContext::getBean);
+            Parent view = loader.load();
+            mainPane.setCenter(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
