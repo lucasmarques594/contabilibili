8ffd7c0da4d1c1b586aaa9eb8edc371967ef8582
lucasmarques594 <demordevil594@gmail.com>
2025-09-26 12:08:32 -0300

    feat(usecase): implement cartorio registration use case
    Add service class with repository dependency and execution method to create and persist new cartorio entities

--- src/main/java/br/com/contabilibili/application/usecases/RegistrarCartorio.java
+++ src/main/java/br/com/contabilibili/application/usecases/RegistrarCartorio.java
@@ -1,5 +1,19 @@
 package br.com.contabilibili.application.usecases;
 
+import br.com.contabilibili.domain.model.Cartorio;
+import br.com.contabilibili.domain.repository.CartorioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class RegistrarCartorio {
-    
+
+    private final CartorioRepository cartorioRepository;
+
+    public Cartorio executar(String nome) {
+        Cartorio novoCartorio = new Cartorio();
+        novoCartorio.setNome(nome);
+        return cartorioRepository.save(novoCartorio);
+    }
 }
