8f23ef4bf93fb7b5e6991fffe76a56ea698d32b6
lucasmarques594 <demordevil594@gmail.com>
2025-09-26 11:52:51 -0300

    feat(model): add entity annotations and fields to Pessoa class
    Add JPA annotations and Lombok getters/setters to enable database persistence. Include id, nome, and tipoPessoa fields to represent basic person attributes.

--- src/main/java/br/com/contabilibili/domain/model/Pessoa.java
+++ src/main/java/br/com/contabilibili/domain/model/Pessoa.java
@@ -1,5 +1,17 @@
 package br.com.contabilibili.domain.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Pessoa {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Enumerated(EnumType.STRING)
+    private TipoPessoa tipoPessoa;
 }
