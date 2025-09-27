b00b1f0f78c0eb164bc1b918950d61d816651aac
lucasmarques594 <demordevil594@gmail.com>
2025-09-27 19:31:50 -0300

    feat(SalvarPessoa): implement save person use case with repository
    Add service annotation and required dependencies to save person entity using PessoaRepository. The change enables persistence of person data in the system.

--- src/main/java/br/com/contabilibili/application/usecases/SalvarPessoa.java
+++ src/main/java/br/com/contabilibili/application/usecases/SalvarPessoa.java
@@ -1,5 +1,16 @@
 package br.com.contabilibili.application.usecases;
 
+import br.com.contabilibili.domain.model.Pessoa;
+import br.com.contabilibili.domain.repository.PessoaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SalvarPessoa {
-    
+    private final PessoaRepository pessoaRepository;
+
+    public Pessoa executar(Pessoa pessoa) {
+        return pessoaRepository.save(pessoa);
+    }
 }
