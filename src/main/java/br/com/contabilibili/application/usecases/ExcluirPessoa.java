90626d0de5a2a198eb05045d3ae37dcc9c0655f2
lucasmarques594 <demordevil594@gmail.com>
2025-09-27 19:32:34 -0300

    feat(usecase): implement pessoa deletion service
    Add service annotation and repository dependency to enable pessoa deletion

--- src/main/java/br/com/contabilibili/application/usecases/ExcluirPessoa.java
+++ src/main/java/br/com/contabilibili/application/usecases/ExcluirPessoa.java
@@ -1,5 +1,15 @@
 package br.com.contabilibili.application.usecases;
 
+import br.com.contabilibili.domain.repository.PessoaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ExcluirPessoa {
-    
+    private final PessoaRepository pessoaRepository;
+
+    public void executar(Long id) {
+        pessoaRepository.deleteById(id);
+    }
 }
