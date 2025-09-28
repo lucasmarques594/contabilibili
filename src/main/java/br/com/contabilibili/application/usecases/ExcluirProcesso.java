129c89208c0332a3aadb3c3ca02ac1f2e87ddaa8
lucasmarques594 <demordevil594@gmail.com>
2025-09-28 11:26:38 -0300

    feat(processo): add delete functionality to ExcluirProcesso
    Implement delete operation using ProcessoRepository to remove processo by ID

--- src/main/java/br/com/contabilibili/application/usecases/ExcluirProcesso.java
+++ src/main/java/br/com/contabilibili/application/usecases/ExcluirProcesso.java
@@ -1,5 +1,15 @@
 package br.com.contabilibili.application.usecases;
 
+import br.com.contabilibili.domain.repository.ProcessoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ExcluirProcesso {
-    
+    private final ProcessoRepository processoRepository;
+
+    public void executar(Long id) {
+        processoRepository.deleteById(id);
+    }
 }
