8e536d1adbe5b9ecdc6727aeb9c4ed21f8c4b1ce
lucasmarques594 <demordevil594@gmail.com>
2025-09-28 11:50:46 -0300

    feat(ExcluirLancamento): implement service to delete financial entry
    Add repository dependency and delete method to handle financial entry deletion

--- src/main/java/br/com/contabilibili/application/usecases/ExcluirLancamento.java
+++ src/main/java/br/com/contabilibili/application/usecases/ExcluirLancamento.java
@@ -1,5 +1,15 @@
 package br.com.contabilibili.application.usecases;
 
+import br.com.contabilibili.domain.repository.LancamentoFinanceiroRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ExcluirLancamento {
-    
+    private final LancamentoFinanceiroRepository repository;
+
+    public void executar(Long id) {
+        repository.deleteById(id);
+    }
 }
