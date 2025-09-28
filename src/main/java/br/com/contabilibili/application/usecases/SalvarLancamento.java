fd52c334f65406f8135bd6505ae9f90d7ebfe35a
lucasmarques594 <demordevil594@gmail.com>
2025-09-28 11:49:21 -0300

    feat: implement saving financial entry use case
    Add service class with repository dependency to save financial entries

--- src/main/java/br/com/contabilibili/application/usecases/SalvarLancamento.java
+++ src/main/java/br/com/contabilibili/application/usecases/SalvarLancamento.java
@@ -1,5 +1,16 @@
 package br.com.contabilibili.application.usecases;
 
+import br.com.contabilibili.domain.model.LancamentoFinanceiro;
+import br.com.contabilibili.domain.repository.LancamentoFinanceiroRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SalvarLancamento {
-    
+    private final LancamentoFinanceiroRepository repository;
+
+    public LancamentoFinanceiro executar(LancamentoFinanceiro lancamento) {
+        return repository.save(lancamento);
+    }
 }
