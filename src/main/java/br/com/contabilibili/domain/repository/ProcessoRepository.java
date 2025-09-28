34544d0620beeee3874ea7ad650abfe07617bed0
lucasmarques594 <demordevil594@gmail.com>
2025-09-28 11:23:26 -0300

    feat(repository): add findAll and deleteById methods to ProcessoRepository

--- src/main/java/br/com/contabilibili/domain/repository/ProcessoRepository.java
+++ src/main/java/br/com/contabilibili/domain/repository/ProcessoRepository.java
@@ -1,10 +1,15 @@
 package br.com.contabilibili.domain.repository;
 
 import br.com.contabilibili.domain.model.Processo;
+import java.util.List;
 import java.util.Optional;
 
 public interface ProcessoRepository {
     Processo save(Processo processo);
 
     Optional<Processo> findById(Long id);
+
+    List<Processo> findAll();
+
+    void deleteById(Long id);
 }
