8b3f8a29a03d5e9b3b1390fcd75a4877e7f04a7d
lucasmarques594 <demordevil594@gmail.com>
2025-09-28 11:47:55 -0300

    feat(repository): add findAll and deleteById methods to LancamentoFinanceiroRepository
    Add methods to retrieve all entries and delete by ID for better data management

--- src/main/java/br/com/contabilibili/domain/repository/LancamentoFinanceiroRepository.java
+++ src/main/java/br/com/contabilibili/domain/repository/LancamentoFinanceiroRepository.java
@@ -1,10 +1,14 @@
 package br.com.contabilibili.domain.repository;
 
 import br.com.contabilibili.domain.model.LancamentoFinanceiro;
+import java.util.List;
 import java.util.Optional;
 
 public interface LancamentoFinanceiroRepository {
     LancamentoFinanceiro save(LancamentoFinanceiro lancamentoFinanceiro);
 
+    List<LancamentoFinanceiro> findAll();
     Optional<LancamentoFinanceiro> findById(Long id);
+
+    void deleteById(Long id);
 }
